3fa2e0d527da02544044b6650240a6f615f806c3
AbhiGit13 <abhishekchaudhari183@gmail.com>
2025-11-18 19:08:20 +0530

    added cart faetaure

--- src/pages/CartPage.java
+++ src/pages/CartPage.java
@@ -5,7 +5,13 @@ public class CartPage {
 	
 	
 	public void cart() {
-		String nameD2="cart dev 2";
-		System.out.println(nameD2);
+//<<<<<<< Updated upstream
+//		String nameD2="cart dev 2";
+//		System.out.println(nameD2);
+//=======   upper code rejected and lower got finalised
+		
+		String nameD1="cart dev 1";
+		System.out.println(nameD1);
+//>>>>>>> Stashed changes
 	}
 }
